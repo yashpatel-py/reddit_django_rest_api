082d3695fa2100198966b97d251a67b1ac24cce5
yashnpatel18 <yashp63544@gmail.com>
2021-06-04 21:45:17 +0530

    redit api

---
+++ posts/views.py
@@ -0,0 +1,55 @@
+from rest_framework.exceptions import ValidationError
+from posts import serializers
+from django.shortcuts import render
+from rest_framework import generics, mixins, permissions, status
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from posts.models import Post, Vote
+from posts.serializers import PostSerializer, VoteSerializer
+
+# Create your views here.
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(poster=self.request.user)
+
+
+class PostRetriveDestroy(generics.RetrieveDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def delete(self, request, *args, **kwargs):
+        post = Post.objects.filter(pk=kwargs['pk'], poster=self.request.user)
+        if post.exists():
+            return self.destroy(request, *args, **kwargs)
+        else:
+            raise ValidationError('This is not our post to delete, do you understand!!.......funny :)')
+
+
+class VoteCreate(generics.CreateAPIView, mixins.DestroyModelMixin):
+    serializer_class = VoteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        post = Post.objects.get(pk=self.kwargs['pk'])
+        return Vote.objects.filter(voter=user, post=post)
+
+    def perform_create(self, serializer):
+        if self.get_queryset().exists():
+            raise ValidationError('You have already voted for this post :)')
+        serializer.save(voter=self.request.user, post=Post.objects.get(pk=self.kwargs['pk']))
+
+    def delete(self, request, *args, **kwargs):
+        if self.get_queryset().exists():
+            self.get_queryset().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        else:
+            raise ValidationError('You never voted for this post....silly!')
